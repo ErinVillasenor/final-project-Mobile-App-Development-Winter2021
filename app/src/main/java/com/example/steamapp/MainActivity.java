595c544e431dc8e6f31c3aadd2695bbf691c7173
Erin Villasenor <villaser@oregonstate.edu>
2021-03-16 19:16:14 -0700

    Possible implementation of db stuff & drawer

--- app/src/main/java/com/example/steamapp/MainActivity.java
+++ app/src/main/java/com/example/steamapp/MainActivity.java
@@ -12,6 +12,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -26,10 +27,14 @@ import android.widget.TextView;
 import com.example.steamapp.data.LoadingStatus;
 import com.example.steamapp.data.PlayerData;
 import com.example.steamapp.data.PlayerSummary;
+import com.example.steamapp.data.SavedPlayer;
 import com.google.android.material.navigation.NavigationView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, SteamPlayerAdapter.OnPlayerClickListener {
+        implements  SharedPreferences.OnSharedPreferenceChangeListener, NavigationView.OnNavigationItemSelectedListener,
+        SteamPlayerAdapter.OnPlayerClickListener, SavedPlayerAdapter.OnSearchResultClickedListener {
 
     private static final String STEAM_API_KEY = BuildConfig.STEAM_API_KEY;
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -40,12 +45,17 @@ public class MainActivity extends AppCompatActivity
     private ProgressBar loadingIndicatorPB;
     private TextView errorMessageTV;
 
+    private SharedPreferences sharedPreferences;
+
+
     private DrawerLayout drawerLayout;
     private RecyclerView drawerRV;
     private SteamPlayerAdapter steamPlayerAdapter;
+    private SavedPlayerAdapter savedPlayerAdapter;
 
     // lifecycle stuff
     private SteamSearchViewModel steamSearchViewModel;
+    private SavedPlayersViewModel savedPlayersViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,6 +84,13 @@ public class MainActivity extends AppCompatActivity
 
         this.steamSearchViewModel = new ViewModelProvider(this)
                 .get(SteamSearchViewModel.class);
+        this.savedPlayersViewModel = new ViewModelProvider(
+                this,
+                new ViewModelProvider.AndroidViewModelFactory(getApplication())
+        ).get(SavedPlayersViewModel.class);
+
+        this.savedPlayerAdapter = new SavedPlayerAdapter(this);
+        this.drawerRV.setAdapter(this.savedPlayerAdapter);
 
         Button searchButton = (Button)findViewById(R.id.btn_search);
         searchButton.setOnClickListener(new View.OnClickListener() {
@@ -83,6 +100,14 @@ public class MainActivity extends AppCompatActivity
                 if (!TextUtils.isEmpty(searchSteamID)) {
                     performSteamIDSearch(STEAM_API_KEY, searchSteamID);
                 }
+            }
+        });
+
+
+        this.savedPlayersViewModel.getAllSavedPlayers().observe(this, new Observer<List<SavedPlayer>>() {
+            @Override
+            public void onChanged(List<SavedPlayer> savedPlayers) {
+                savedPlayerAdapter.updateSavedPlayersData(savedPlayers);
             }
         });
 
@@ -131,11 +156,19 @@ public class MainActivity extends AppCompatActivity
         );
     }
 
+
+    @Override
+    public void onSearchResultClicked(SavedPlayer savedPlayer) {
+        SharedPreferences.Editor sharedPreferencesEditor = this.sharedPreferences.edit();
+        sharedPreferencesEditor.putString(getString(R.string.pref_player_key), savedPlayer.name);
+        sharedPreferencesEditor.apply();
+    }
+
     private void performSteamIDSearch(String API_KEY, String playerID){
         Log.d(TAG, "== user entered this steamID: " + playerID);
         steamSearchViewModel.loadPlayerSearchResults(API_KEY, playerID);
     }
-
+//key: 76561197960435530
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
@@ -175,10 +208,22 @@ public class MainActivity extends AppCompatActivity
                 return false;
         }
     }
-
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        long timestamp = (long) System.currentTimeMillis();
+        SavedPlayer savedPlayer = new SavedPlayer(this.sharedPreferences.getString(
+                getString(R.string.pref_player_key),
+                "Robin"
+        ),  "NULL", timestamp);
+        savedPlayersViewModel.insertPlayer(savedPlayer);
+        //   loadingStatus.setValue(LoadingStatus.SUCCESS);
+       // this.loadForecast(); //Our equivalent?
+    }
+//key 76561197960435530
     @Override
     public void onPlayerClick(PlayerSummary playerSummary) {
         Intent intent = new Intent(this, FriendsActivity.class);
         startActivity(intent);
+
     }
 }
